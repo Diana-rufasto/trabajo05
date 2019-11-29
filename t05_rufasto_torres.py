4bbc3e656f0d2cfe64a2a4d03ccce5a1a09faa78
Diana-rufasto <57873525+diana-rufasto@users.noreply.github.com>
2019-11-29 14:33:33 -0500

    Se agrego el 2do ejercicio

--- t05_rufasto_torres.py
+++ t05_rufasto_torres.py
@@ -9,37 +9,5 @@ print("variable promedio de un alumno es:",type_promedio_de_un_alumno)
 print("variable es float?:",promedio_de_un_alumno_es_float)
 print("variable es str?:",promedio_de_un_alumno_es_str)
 
-#EJERCICIO02
-IMC_con_calificacion_sobrepeso= 26.99933
-type_IMC_con_calificacion_sobrepeso=type(IMC_con_calificacion_sobrepeso)
-
-IMC_con_calificacion_sobrepeso_es_float=isinstance(IMC_con_calificacion_sobrepeso,float)
-IMC_con_calificacion_sobrepeso_es_int=isinstance(IMC_con_calificacion_sobrepeso,int)
-
-print("variable IMC con calificacion sobrepeso es:",type_IMC_con_calificacion_sobrepeso)
-print("variable es float?:",IMC_con_calificacion_sobrepeso_es_float)
-print("variable es int?:",IMC_con_calificacion_sobrepeso_es_int)
-
-#EJERCICIO03
-IMC_con_calificacion_delgadez_severa=15.0342
-type_IMC_con_calificacion_delgadez_severa=type(IMC_con_calificacion_delgadez_severa)
-
-IMC_con_calificacion_delgadez_severa_es_float=isinstance(IMC_con_calificacion_delgadez_severa,float)
-IMC_con_calificacion_delgadez_severa_es_bool=isinstance(IMC_con_calificacion_delgadez_severa,int)
-
-print("variable IMC con calificacion delgadez severa es:",type_IMC_con_calificacion_delgadez_severa)
-print("variable es float?:",IMC_con_calificacion_delgadez_severa_es_float)
-print("variable es bool?:",IMC_con_calificacion_delgadez_severa_es_bool)
-
-#EJERCICIO04
-dias_de_vacaciones=15
-type_dias_de_vacaciones=type(dias_de_vacaciones)
-
-dias_de_vacaciones_es_int=isinstance(dias_de_vacaciones,int)
-dias_de_vacaciones_es_str=isinstance(dias_de_vacaciones,str)
-
-print("variable dias de vacaciones es:",type_dias_de_vacaciones)
-print("variable es int?:",dias_de_vacaciones_es_int)
-print("variable es str?:",dias_de_vacaciones_es_str)
 
 
