4b15351561a05b8a82e12c57ae78d9b04f6f59ad
Diana-rufasto <57873525+diana-rufasto@users.noreply.github.com>
2019-11-29 11:42:54 -0500

    Se egrego el 1er ejercicio

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # trabajo05
 Trabajo Nro 5
+
+#INTEGRANTES
+Rufasto Torres Diana
+Llontop Atencio Ena 
---
+++ t05_rufasto_torres.py
@@ -0,0 +1,13 @@
+#EJERCICIO01
+promedio_de_un_alumno= 14.05
+type_promedio_de_un_alumno=type(promedio_de_un_alumno)
+
+promedio_de_un_alumno_es_float=isinstance(promedio_de_un_alumno,float)
+promedio_de_un_alumno_es_str=isinstance(promedio_de_un_alumno,str)
+
+print("variable promedio de un alumno es:",type_promedio_de_un_alumno)
+print("variable es float?:",promedio_de_un_alumno_es_float)
+print("variable es str?:",promedio_de_un_alumno_es_str)
+
+
+
